304c9829a510f249c2afb7dc850f929a3d53f2ca
Souvik Chakraborty <souvikc3030@gmail.com>
2022-08-15 23:11:50 +0530

    pagination added

--- blog/main.py
+++ blog/main.py
@@ -4,7 +4,7 @@ from datetime import  datetime
 import json
 import os
 from werkzeug.utils import secure_filename
-
+import math
 
 
 app = Flask(__name__)
@@ -44,8 +44,25 @@ class Posts(db.Model):
 
 @app.route('/')
 def home():
-    posts = Posts.query.filter_by().all()[0:params['no_of_post']]
-    return render_template('/index.html', params=params, posts=posts)
+    posts = Posts.query.filter_by().all()
+    last = math.ceil(len(posts) / int(params['no_of_post']))
+    page = request.args.get('page')
+    if (not str(page).isnumeric()):
+        page = 1
+    page = int(page)
+    posts = posts[(page-1)*int(params['no_of_post']):(page-1)*int(params['no_of_post'])+ int(params['no_of_post'])]
+    # posts = posts[(page-1)*int(params['no_of_post']):(page-1)*int(params['no_of_post'])+int(params['no_of_post'])]
+    if page==1:
+        prev = '#'
+        next = '/?page=' + str(page+1)
+    elif page==last:
+        prev = '/?page=' + str(page-1)
+        next = '#'
+    else:
+        prev = '/?page=' + str(page-1)
+        next = '/?page=' + str(page+1)
+    # posts = Posts.query.filter_by().all()[0:params['no_of_post']]
+    return render_template('/index.html', params=params, posts=posts, prev=prev, next=next)
 
 
 @app.route('/about')
